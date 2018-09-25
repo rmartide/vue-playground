d2daf867d16977d1d6f694b49d0205a058647917
Rodrigo Martín de Lamo <rmartin@devcenter.es>
2018-09-25 16:22:37 +0200

    Logo animation modified

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,8 @@ export default {
     Forms
   },
   mounted: function () {
-    TweenMax.to("#logo", 2, {x:1200, rotation:360, ease:Bounce.easeOut, scale:0.5, backgroundColor: 'red', border:'10px solid black', borderRadius:10, padding:30});
-    TweenMax.to("#logo", 3, {x:0, rotation:-360, ease:Elastic.easeOut.config(1, 0.3), scale:1, delay: 2.5, backgroundColor: 'yellow', borderColor:'red'});
+    TweenMax.to("#logo", 2, {x:1400, rotation:360, ease:Bounce.easeOut, scale:0.5, backgroundColor: 'red', border:'10px solid black', borderRadius:10, padding:30});
+    TweenMax.to("#logo", 3, {x:700, rotation:-360, ease:Elastic.easeOut.config(1, 0.3), scale:0.3, delay: 2.5, backgroundColor: 'black', borderColor:'red'});
   }
 }
 </script>
