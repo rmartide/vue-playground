a4f8b03e144ef148c7fc1d69a173dd99bea6895a
Rodrigo Martín de Lamo <rmartin@devcenter.es>
2018-09-26 16:51:15 +0200

    Added animation when task is added

--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,14 +1,25 @@
 <template>
-    <li :id="'task-' + task.id">
-        <input type="checkbox" @change="$emit('deltask', task);"> {{task.text}}
+    <transition name="custom-classes-transition" enter-active-class="animated rubberBand">
+        <li :id="'task-' + task.id" v-if="show">
+            <input type="checkbox" @change="$emit('deltask', task);"> {{task.text}}
     </li>
+    </transition>
 </template>
 
 <script>
+
 export default {
     name: 'TodoListItem',
+    data() {
+        return {
+            show: false
+        }
+    },
     props: {
         task: Object
+    },
+    mounted() {
+        this.show = true;
     }
 }
 </script>
