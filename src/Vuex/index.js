15b987390d612cf0dfd2375bf01f655f24980ca5
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-06-27 21:37:17 +0200

    Added vue.config.js to change the public path on build

--- src/Vuex/index.js
+++ src/Vuex/index.js
@@ -9,7 +9,7 @@ const store = new Vuex.Store({
     state: {
         count: 0,
         tasks: [
-            { text: 'Adding animation when the task is added', id: idCounter++ },
+            { text: 'Add animation when the task is added', id: idCounter++ },
             { text: 'Use a checkbox on the left instead of a button', id: idCounter++ },
             { text: 'Extend the input and add some css', id: idCounter++ },
             { text: 'Try to add a delete animation with transition-group', id: idCounter++ }
